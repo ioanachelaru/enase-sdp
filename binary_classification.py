85df33ecf9c84c9ebb8f6a5b2668c4a22684f3cd
Ioana Chelaru <ioanachelaru05@gmail.com>
2023-12-12 10:38:20 +0200

    Wip

--- binary_classification.py
+++ binary_classification.py
@@ -3,9 +3,12 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import classification_report, confusion_matrix
+# from sklearn.ensemble import RandomForestClassifier
 
-NO_EPOCHS = 50
-BATCH_SIZE = 64
+NO_EPOCHS = 10
+BATCH_SIZE = 32
 LOSS = 'binary_crossentropy'
 OPTIMIZER = 'adam'
 METRICS = ['accuracy']
@@ -20,14 +23,19 @@ def create_model(input_dim):
     model.add(Dense(1, activation=ACTIVATION[1]))  # Output layer
 
     model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
+
     return model
 
 
 def train_model(no_clusters, target_cluster, model, x_train, y_train, x_val, y_val):
-    class_weights = {0: 1, 1: 100}
-    sample_weights = np.array([class_weights[label] for label in y_train.iloc[:, 0]])
 
-    history = model.fit(x_train, y_train, validation_data=(x_val, y_val), sample_weight=sample_weights, 
+    # Calculate class weights
+    class_weights = compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+
+    # Create a dictionary to pass to the model
+    class_weight = {i: w for i, w in enumerate(class_weights)}
+
+    history = model.fit(x_train, y_train, validation_data=(x_val, y_val), class_weight=class_weight, 
                         epochs=NO_EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
     
     plt.figure()
@@ -60,17 +68,26 @@ def binary_classification(no_clusters, target_cluster, filename):
 
     model = create_model(60)
     model = train_model(no_clusters, target_cluster, model, x_train, y_train, x_val, y_val)
-    score = model.evaluate(x_test, y_test, verbose=VERBOSE)
-    print("Test score:", score[0])
-    print("Test accuracy:", score[1])
+    loss, accuracy = model.evaluate(x_test, y_test, verbose=VERBOSE)
+    print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
+    
+    # Make predictions on the test set
+    predictions = model.predict(x_test)
 
-    # return model
+    # Threshold predictions to convert probabilities to binary predictions
+    binary_predictions = (predictions > 0.5).astype(int)
+
+    # Print classification report and confusion matrix
+    print("Classification Report:\n", classification_report(y_test, binary_predictions))
+    print("Confusion Matrix:\n", confusion_matrix(y_test, binary_predictions))
+
+    return model
 
 if __name__ == '__main__':
     binary_classification(4, 1, 'data_to_cluster/4_clusters/data_cluster_1/')
     # for i in range(4, 7):
     #     print(f'Building model for {i} clusters...')
         
-    #     for ii in range(1, i+1):
+    #     for ii in range(0, i):
     #         print(f'...building model for cluster {ii}...')
     #         binary_classification(no_clusters, target_cluster, f'data_to_cluster/{i}_clusters/data_cluster_{ii}/')
